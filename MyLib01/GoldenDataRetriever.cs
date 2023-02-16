b3abeab7bea85dade24bee58a44a304581c1a34e
Philippe BONS <philippe.bons@sam-assurance.com>
2023-02-16 13:57:03 +0100

    Initialisation prj

---
+++ MyLib01/GoldenDataRetriever.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib01
+{
+    public class GoldenDataRetriever
+    {
+
+        public static string retrieveStuff(string someInput)
+        {
+            return "Fool's Gold";
+        }
+
+    }
+}
